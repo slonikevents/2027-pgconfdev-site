f03003b42b2c88d724cca29e6ae087289afdb272
Steve Singer <steve@ssinger.info>
2026-09-05 15:27:12 -0400

    update hotel markup

--- site/(main)/attend/travel/+page.svelte
+++ site/(main)/attend/travel/+page.svelte
@@ -153,7 +153,7 @@
 
 <section aria-label="Partner hotels" class="(main)">
   <div class="grid+14">
-    <div class="note border">
+    <div class="main band">
       <hgroup class="h5">
         <h3>Alt Hotel Montreal</h3>
         <p class="mono action">$239+Tax(19%) Rate valid till April 8 2027</p>
@@ -162,10 +162,16 @@
         <a href="https://www.germainhotels.com/en/alt-hotel/montreal"
           >The Alt Hotel</a
         >
-        is located at 120, rue Peel, approximately a 15 minute walk from the
-        conference venue. A room with a queen bed is $239+tax(19%) , a corner
-        room with a queen bed is $269+tax(19%) and a room with two queen beds is
-        $259+tax(19%). You can book
+        is located at 120, rue Peel, approximately a 15 minute walk from the conference
+        venue.
+      </p>
+      <ul>
+        <li>A room with a queen bed is $239+tax(19%).</li>
+        <li>A corner room with a queen bed is $269+tax(19%).</li>
+        <li>A room with two queen beds is $259+tax(19%).</li>
+      </ul>
+      <p>
+        You can book
         <a
           href="https://reservation.germainhotels.com/ibe/details.aspx?propertyid=17532&nights=7&checkin=05/08/2027&group=2705POSTGR&lang=en-us&adults=1&childAges="
         >
