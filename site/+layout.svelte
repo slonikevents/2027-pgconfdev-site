52407198a0221253aa8639d973a41198274db85f
Kaiting Chen <ktchen14@gmail.com>
2026-09-17 09:44:31 -0400

    Enable Cloudflare Web Analytics

--- site/+layout.svelte
+++ site/+layout.svelte
@@ -2,6 +2,7 @@
   import newsreaderLatinOpszNormalWoff2 from "@fontsource-variable/newsreader/files/newsreader-latin-opsz-normal.woff2?url";
   import publicSansLatinWghtNormalWoff2 from "@fontsource-variable/public-sans/files/public-sans-latin-wght-normal.woff2?url";
   import { setLucideProps } from "@lucide/svelte";
+  import { CLOUDFLARE_TOKEN } from "$app/env/public";
   import type { LayoutProps } from "./$types";
   import "./site.css";
   import "virtual:uno.css";
@@ -19,6 +20,7 @@
     href={publicSansLatinWghtNormalWoff2}
     crossorigin="anonymous"
   />
+
   <link
     rel="preload"
     as="font"
@@ -26,6 +28,12 @@
     href={newsreaderLatinOpszNormalWoff2}
     crossorigin="anonymous"
   />
+
+  {#if CLOUDFLARE_TOKEN !== undefined}
+    {const source = "https://static.cloudflareinsights.com/beacon.min.js"}
+    {const beacon = JSON.stringify({ token: CLOUDFLARE_TOKEN })}
+    <script type="module" src={source} data-cf-beacon={beacon}></script>
+  {/if}
 </svelte:head>
 
 {@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,9 @@ export default {
   kit: {
     adapter: adapter(),
     appDir: "svelte",
+    experimental: {
+      explicitEnvironmentVariables: true,
+    },
     files: {
       appTemplate: "site.html",
       hooks: {
